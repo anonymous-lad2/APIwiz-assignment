7a6a56aa98ae2911752a5702bd2f4e79c391fe01
anonymous-lad2 <anonymouslad540@gmail.com>
2025-05-06 05:22:39 +0530

    graph traversal problem

---
+++ GraphTraversal.java
@@ -0,0 +1,8 @@
+public class GraphTraversal {
+
+    public static void main(String[] args) {
+
+        System.out.print("goal: ");
+        System.out.println("Traverse the entire graph starting from the root node, ensuring each node is executed once");
+    }
+}
